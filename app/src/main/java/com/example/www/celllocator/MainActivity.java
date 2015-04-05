027ae2a20bd4ffeb744473c1e60f30ab42cf2d7d
raghu272 <dasari.10@osu.edu>
2015-04-05 11:49:11 -0800

    raghu comm update

--- app/src/main/java/com/example/www/celllocator/MainActivity.java
+++ app/src/main/java/com/example/www/celllocator/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.www.celllocator;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.util.Locale;
 
-
 public class MainActivity extends ActionBarActivity {
 
-    EditText id, lac;
+    EditText id, lac,mnc,mcc;
     Button update, locate;
+    String cellmcc,cellmnc;  //Mobile Country Code
+    String cellid,celllac; //Cell ID
 
+    Boolean error;
+    String strURLSent;
+    String GetOpenCellID_fullresult;
+
+    String myLatitude, myLongitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,47 +42,95 @@ public class MainActivity extends ActionBarActivity {
 
         id = (EditText) findViewById(R.id.edit_cellid);
         lac = (EditText) findViewById(R.id.edit_lac);
+        mcc = (EditText) findViewById(R.id.edit_mcc);
+        mnc = (EditText) findViewById(R.id.edit_mnc);
         update = (Button) findViewById(R.id.cmd_update);
         locate = (Button) findViewById(R.id.cmd_locateme);
 
         //Set CELL_ID and LAC fields to their respective values
         update.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {
+            public void onClick(View v) {
 
                 //TODO: Will need to call function that gets these values
                 //Used on so app will run
-                String cellid = "test cell id";
-                String celllac = "test cell lac";
+
+                TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager.getCellLocation();
+                String networkOperator = telephonyManager.getNetworkOperator();
+                cellmcc = networkOperator.substring(0, 3);
+                cellmnc = networkOperator.substring(3);
+                final TelephonyManager telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                if (telephony.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+                    final GsmCellLocation location = (GsmCellLocation) telephony.getCellLocation();
+                    if (location != null) {
+                        cellid = Integer.toString(location.getLac());
+                        celllac = Integer.toString(location.getCid());
+                    }
+                }
                 id.setText(cellid);
                 lac.setText(celllac);
+                mcc.setText(cellmcc);
+                mnc.setText(cellmnc);
             }
         });
 
         //Use CELL_ID and LAC to obtain longitude and latitude values. Will use these values to
         //locate physical location of phone
         locate.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {
+            public void onClick(View v) {
 
                 Double longitude, latitude;
-
+                try {
+                    GetOpenCellID();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 //TODO: Will need to push these values to function in order to get long/lat
-                EditText cid = (EditText) findViewById(R.id.edit_cellid);
-                String cellid = cid.getText().toString();
-                EditText clac = (EditText) findViewById(R.id.edit_lac);
-                String celllac = clac.getText().toString();
 
                 //Used only so app runs
-                longitude = 30.409456;
-                latitude = 59.915494;
 
                 //TODO: Will need to open map and locate phone using long/lat.
                 //This shows location, but doesn't have a marker/pin
-                String map = String.format(Locale.ENGLISH, "geo:%f,%f", latitude, longitude);
+                final LatLng x;
+                x = new LatLng((double)Integer.parseInt(myLatitude),Integer.parseInt(myLongitude));
+                String map = String.format(Locale.ENGLISH, "geo:%f,%f", myLatitude, myLongitude);
+
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
                 startActivity(intent);
 
             }
+
         });
+    }
+    public String getGetOpenCellID_fullresult(){
+        return GetOpenCellID_fullresult;
+    }
+    public void GetOpenCellID() throws Exception {
+        groupURLSent();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(strURLSent);
+        HttpResponse response = client.execute(request);
+        GetOpenCellID_fullresult = EntityUtils.toString(response.getEntity());
+        spliteResult();
+    }
+    public void groupURLSent(){
+        strURLSent =
+                "http://www.opencellid.org/cell/get?mcc=" + mcc
+                        +"&mnc=" + mnc
+                        +"&cellid=" + cellid
+                        +"&lac=" + lac
+                        +"&fmt=txt";
+    }
+    private void spliteResult(){
+        if(GetOpenCellID_fullresult.equalsIgnoreCase("err")){
+            error = true;
+        }else{
+            error = false;
+            String[] tResult = GetOpenCellID_fullresult.split(",");
+            myLatitude = tResult[0];
+            myLongitude = tResult[1];
+        }
+
     }
 
 
