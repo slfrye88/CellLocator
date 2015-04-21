77d7ead472026a74f72b616b647fbd9159a38714
raghu272 <dasari.10@osu.edu>
2015-04-21 06:24:13 -0800

    secure url

--- app/src/main/java/com/example/www/celllocator/MapsActivity.java
+++ app/src/main/java/com/example/www/celllocator/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.www.celllocator;
 
-import android.support.v4.app.FragmentActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -10,12 +16,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+    private String uuid="0cb564b6-e61a-11e4-8a00-1681e6b88ec1";
+    LatLng x;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    BluetoothAdapter mBluetoothAdapter;
+    private Handler handler1;
+    private BluetoothSocket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        Button b = (Button) findViewById(R.id.check);
         setUpMapIfNeeded();
     }
 
@@ -63,7 +76,19 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         Bundle bundle = getIntent().getParcelableExtra("bundle");
-        LatLng x = bundle.getParcelable("x");
+        x = bundle.getParcelable("x");
+
         mMap.addMarker(new MarkerOptions().position(x).title("Marker"));
     }
+
+    public void sendMessage(View view) {
+
+        Bundle args = new Bundle();
+        args.putParcelable("x", x);
+        Intent intent = new Intent(this, BluetoothChat.class);
+        intent.putExtra("bundle", args);
+        startActivity(intent);
+    }
+
+
 }
