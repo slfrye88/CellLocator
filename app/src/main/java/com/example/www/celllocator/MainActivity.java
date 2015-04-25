aa247dccb45512782b1ce491d12aeb5480bf01b1
raghu272 <dasari.10@osu.edu>
2015-04-25 13:58:36 -0800

    added wifi location

--- app/src/main/java/com/example/www/celllocator/MainActivity.java
+++ app/src/main/java/com/example/www/celllocator/MainActivity.java
@@ -12,20 +12,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
     public static final String TAG = "MYTAG";
     EditText id, lac,mnc,mcc;
-    Button update, locate;
+    Button update, locate,wifi;
     String cellmcc,cellmnc;  //Mobile Country Code
     String cellid,celllac; //Cell ID
 
@@ -34,6 +38,14 @@ public class MainActivity extends ActionBarActivity {
     String GetOpenCellID_fullresult;
 
     String myLatitude, myLongitude;
+    class LoadListener{
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processHTML(String html)
+        {
+            Log.e("result",html);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
         mnc = (EditText) findViewById(R.id.edit_mnc);
         update = (Button) findViewById(R.id.cmd_update);
         locate = (Button) findViewById(R.id.cmd_locateme);
-
+        wifi = (Button) findViewById(R.id.cmd_wifi);
         //Set CELL_ID and LAC fields to their respective values
         update.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -70,13 +82,15 @@ public class MainActivity extends ActionBarActivity {
                 lac.setText(celllac);
                 mcc.setText(cellmcc);
                 mnc.setText(cellmnc);
+
+
             }
         });
 
 
     }
     //just to push
-    public void sendMessage(View view) {
+    public void sendMessage(View view) throws IOException {
 
         Connect con = new Connect();
         con.doInBackground();
@@ -129,6 +143,78 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    public void sendMessage1(View view) throws IOException {
+
+        Connect1 con=new Connect1();
+        con.doInBackground();
+        URL link = new URL("http://freegeoip.net/csv/"+con.Ip);
+
+        HttpURLConnection connection = (HttpURLConnection) link.openConnection();
+        connection.connect();
+
+        InputStream is = connection.getInputStream();
+
+        int status = connection.getResponseCode();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String latlong=null;
+        String line = reader.readLine();
+        String[] fields=line.split(",");
+
+        myLatitude=fields[8];
+        myLongitude=fields[9];
+        Log.v("latitude",myLatitude);
+        Log.v("longitude",myLongitude);
+        double l1 = Double.parseDouble(myLatitude);
+        double l2 = Double.parseDouble(myLongitude);
+        Log.v(TAG, "here"+myLatitude);
+        //Log.v(TAG, myLatitude);
+        LatLng x = new LatLng(l1,l2);
+        Bundle args = new Bundle();
+        args.putParcelable("x", x);
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra("bundle", args);
+        startActivity(intent);
+          /*  int index = nthOccurrence(line, ',', 7);
+            latlong=line.substring(index+1);
+
+              Log.v("newloc",latlong.substring(0,latlong.length()-4));
+        is.close();
+        int latind=nthOccurrence(latlong,',',1);
+        myLatitude=latlong.substring(0,latind);
+        myLongitude=latlong.substring(latind+1);*/
+    }
+
+    private class Connect1 extends AsyncTask<String, Void, String> {
+        public String Ip;
+        @Override
+        protected String doInBackground(String... params) {
+
+            //INSERT YOUR FUNCTION CALL HERE
+            try {
+                strURLSent = new URL("http://www.iplocation.net/");
+                HttpURLConnection urlConnection = (HttpURLConnection) strURLSent.openConnection();
+
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                int ch;
+                StringBuffer sb = new StringBuffer();
+                while ((ch = in.read()) != -1) {
+                    sb.append((char) ch);
+                }
+                String response = sb.toString();
+                Ip=response.substring(response.lastIndexOf("color='green'>")+14,response.lastIndexOf("</font></b>.<br />"));
+                Log.v(TAG, Ip);
+
+                urlConnection.disconnect();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "Executed!";
+
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
